29390c03a59dc8cc9462f53e16ad1b2379bf967e
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-04 07:57:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,8 +14,8 @@ def monopoly_with_second_tier
   # complexity.
   hash = {
     :railroads => { 
-      :pieces => 4
-      :rent_in_dollars => {}
+      :pieces => 4,
+      :rent_in_dollars => {},
       :names => {}
     }
   }
