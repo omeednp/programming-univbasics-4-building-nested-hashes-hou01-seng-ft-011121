494e86d1f4f48f9593e1ad79da350fd4db323b93
Omeed Naraghi-Pour <omeed.naraghipour@gmail.com>
2021-01-04 07:55:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,9 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   hash = {
-    :railroads => { pieces: 4
+    :railroads => { :pieces 4
+                    :rent_in_dollars {}
+                    :names {}
     }
   }
 end
